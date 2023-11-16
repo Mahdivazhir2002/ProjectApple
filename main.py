920f5fe8c2d0e4994cad63d3dcd0977db3365df1
Mahdivazhir <113779354+Mahdivazhir2002@users.noreply.github.com>
2023-11-16 19:23:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ df1 = pd.read_csv('all.csv')
 #create new row
 df1['Candle']='Down'
 df1.loc[df1['Open']<df1['Close'],'Candle']='Up'   
-print(df1)
 #finish work and create  new csv file write this 
 file = open('finall.csv','w')
 df1.to_csv('finall.csv')
