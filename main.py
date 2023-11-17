94d443a8bea11d902e95e72ebcf2f189568d8964
Mahdivazhir <113779354+Mahdivazhir2002@users.noreply.github.com>
2023-11-18 01:10:42 +0330

    Edit Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
+#import needs library for project
 import yfinance as yf
 import pandas as pd
 import datetime
 import csv 
+#get data about Apple
 symbol = "AAPL"
 start_date = datetime.datetime.now() - datetime.timedelta(days=365)
 end_date = datetime.datetime.now()
 
 data = yf.download(symbol, start=start_date, end=end_date, interval="1d")
-
-# افزودن ستون جدید به DataFrame
+#convert to datafream
 datafream=pd.DataFrame(data)
 
-
-
+#save recive datafream in csv file 
+CreateFile = open('all.csv','w')
+datafream.to_csv('all.csv')
 df1 = pd.read_csv('all.csv')
 
-
-
-
-#create new row
+#create new row name=candle and value default is down
 df1['Candle']='Down'
 df1.loc[df1['Open']<df1['Close'],'Candle']='Up'   
 #finish work and create  new csv file write this 
