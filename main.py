37034f382743ecaa9c9984fe050ef9616c854686
Mahdivazhir <113779354+Mahdivazhir2002@users.noreply.github.com>
2023-11-18 01:29:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ datafream=pd.DataFrame(data)
 CreateFile = open('all.csv','w')
 datafream.to_csv('all.csv')
 df1 = pd.read_csv('all.csv')
-
+CreateFile.close()
 #create new row name=candle and value default is down
 df1['Candle']='Down'
 df1.loc[df1['Open']<df1['Close'],'Candle']='Up'   
 #finish work and create  new csv file write this 
 file = open('finall.csv','w')
 df1.to_csv('finall.csv')
+file.close()
